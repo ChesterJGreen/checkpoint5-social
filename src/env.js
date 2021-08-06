874c98411c42bafc7ef05593bae536754c40aa88
cjgreenie <seejarom@gmail.com>
2021-08-06 11:29:02 -0600

    beginning

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const baseURL = dev ? 'https://bcw-sandbox.herokuapp.com/' : ''
+export const domain = 'codeworksclassroom.auth0.com'
+export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
+export const audience = 'https://codeworksclassroom.com'
