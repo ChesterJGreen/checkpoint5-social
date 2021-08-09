0ef61e66da017b38a9d2d5e8e03082614cfb9e1d
cjgreenie <seejarom@gmail.com>
2021-08-08 21:40:14 -0600

    adjusting posts pages

--- src/services/PostsService.js
+++ src/services/PostsService.js
@@ -9,8 +9,14 @@ class PostsService {
     AppState.posts = res.data
   }
 
+  async getAllPage(nextPage) {
+    const res = await api.get('api/posts?page=' + nextPage)
+    logger.log(res.data)
+    AppState.posts = res.data
+  }
+
   async getByProfileId(id) {
-    const res = await api.get()
+    const res = await api.get(`/api/posts/${id}/posts`)
     logger.log(res.data)
   }
 
@@ -23,6 +29,12 @@ class PostsService {
   async destroy(id) {
     await api.delete('api/posts' + id)
     AppState.posts = AppState.posts.filter(p => p.id !== id)
+  }
+
+  async like(id) {
+    const editedPost = AppState.posts.likes++
+    const res = await api.put('api/posts' + id, editedPost)
+    AppState.posts = res.data
   }
 }
 export const postsService = new PostsService()
